e7f09f6f0b97f315bf9136ef1faf17e3b9f0e34c
skynettw <skynet.tw@gmail.com>
2023-04-20 14:16:18 +0800

    add.php final

--- add.php
+++ add.php
@@ -8,9 +8,6 @@ $name = $_POST["name"];
 $h = $_POST["height"];
 $w = $_POST["weight"];
 
-if ($name=="" || $h=="" || $w=="") {
-    die("你要輸入資料才行！");
-}
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -21,11 +18,6 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO bodyinfo (name, h, w) VALUES ('" . 
       $name . "'," . $h . "," . $w . ")";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
+$conn->query($sql);
 $conn->close();
-?>
+header("Location: bodyinfo.php");
--- bodyinfo.php
+++ bodyinfo.php
@@ -14,27 +14,27 @@ if ($conn->connect_error) {
 <h1>班級健康管理</h1>
 <?php include "header.php"; ?>
 <form action="add.php" method="POST">
-    姓名：<input type=text name=name size=10 required><br>
-    身高：<input type=text name=height size=5 required>公分<br>
-    體重：<input type=text name=weight size=5 required>公斤<br>
-    <input type=submit value="新增">
+  姓名：<input type=text name=name size=10 required><br>
+  身高：<input type=text name=height size=5 required>公分<br>
+  體重：<input type=text name=weight size=5 required>公斤<br>
+  <input type=submit value="新增">
 </form>
 <hr>
 <?php
-$sql = "SELECT * FROM bodyinfo";
+$sql = "SELECT * FROM bodyinfo order by h desc";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   echo "<table border=1>";
   echo "<tr><td>編號</td><td>姓名</td><td>身高</td><td>體重</td><td>管理</td></tr>";
-  while($row = $result->fetch_assoc()) {
+  while ($row = $result->fetch_assoc()) {
     echo "<tr>";
-    echo "<td>" . $row["id"] . "</td>" . 
-         "<td>" . $row["name"] . "</td>" .
-         "<td>" . $row["h"] . "</td>" .
-         "<td>" . $row["w"] . "</td>" .
-         "<td>刪除</td>" .
-         "</tr>";
+    echo "<td>" . $row["id"] . "</td>" .
+      "<td>" . $row["name"] . "</td>" .
+      "<td>" . $row["h"] . "</td>" .
+      "<td>" . $row["w"] . "</td>" .
+      "<td>刪除</td>" .
+      "</tr>";
   }
   echo "</table>";
 } else {
