481b6f0f12c6672297d48bf638a71481302fef61
skynettw <skynet.tw@gmail.com>
2023-04-20 14:05:40 +0800

    add.php version 2

--- add.php
+++ add.php
@@ -4,6 +4,13 @@ $username = "root";
 $password = "12345678";
 $dbname = "mydb";
 
+$name = $_POST["name"];
+$h = $_POST["height"];
+$w = $_POST["weight"];
+
+if ($name=="" || $h=="" || $w=="") {
+    die("你要輸入資料才行！");
+}
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -11,8 +18,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO bodyinfo (name, h, w)
-VALUES ('ABC', 199, 85)";
+$sql = "INSERT INTO bodyinfo (name, h, w) VALUES ('" . 
+      $name . "'," . $h . "," . $w . ")";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
--- bodyinfo.php
+++ bodyinfo.php
@@ -14,9 +14,9 @@ if ($conn->connect_error) {
 <h1>班級健康管理</h1>
 <?php include "header.php"; ?>
 <form action="add.php" method="POST">
-    姓名：<input type=text name=name size=10><br>
-    身高：<input type=text name=height size=5>公分<br>
-    體重：<input type=text name=weight size=5>公斤<br>
+    姓名：<input type=text name=name size=10 required><br>
+    身高：<input type=text name=height size=5 required>公分<br>
+    體重：<input type=text name=weight size=5 required>公斤<br>
     <input type=submit value="新增">
 </form>
 <hr>
