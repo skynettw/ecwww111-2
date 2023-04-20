1f4c4cd0d3ea714bf0f502b1e5f6aafc5e9125e0
skynettw <skynet.tw@gmail.com>
2023-04-20 13:55:48 +0800

    add.php

---
+++ add.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345678";
+$dbname = "mydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO bodyinfo (name, h, w)
+VALUES ('ABC', 199, 85)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
