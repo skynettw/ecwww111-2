2e8a1d33390d6bdef040d90a8a0e3bae79eee4fd
skynettw <skynet.tw@gmail.com>
2023-05-18 15:02:55 +0800

    fix tvlist.php

--- tvlist.php
+++ tvlist.php
@@ -15,36 +15,30 @@ if ($conn->connect_error) {
     <?php include "header.php"; ?>
     <?php if ($_SESSION["user"] == "administrator") {
     ?>
-        <form action="add.php" method="POST">
-            姓名：<input type=text name=name size=10 required><br>
-            身高：<input type=text name=height size=5 required>公分<br>
-            體重：<input type=text name=weight size=5 required>公斤<br>
+        <form action="addtv.php" method="POST">
+            新聞台名稱：<input type=text name=title size=50 required><br>
+            新聞台的vid：<input type=text name=vid size=10 required><br>
             <input type=submit value="新增">
         </form>
     <?php } ?>
     <hr>
     <?php
-    $sql = "SELECT * FROM bodyinfo order by h desc";
+    $sql = "SELECT * FROM tvinfo;";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         echo "<table class='table table-striped'>";
-        echo "<tr><td>編號</td><td>姓名</td><td>身高</td><td>體重</td><td>管理</td></tr>";
+        echo "<tr><td>編號</td><td>電視台名稱</td><td>VID</td><td>管理</td></tr>";
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row["id"] . "</td>" .
-                "<td>" . $row["name"] . "</td>" .
-                "<td>" . $row["h"] . "</td>" .
-                "<td>" . $row["w"] . "</td>";
+                "<td>" . $row["title"] . "</td>" .
+                "<td>" . $row["vid"] . "</td>";
 
             if ($_SESSION["user"] == "administrator") {
-                echo "<td><a href='del.php?id="
+                echo "<td><a href='deltv.php?id="
                     . $row["id"] . "' class='btn btn-outline-danger btn-sm'>刪除</a></td>";
             }
-
-
-
-
             echo "</tr>";
         }
         echo "</table>";
